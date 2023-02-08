3dc607819f9684a93039bee666811aa7e817babc
AlonRoz <87911114+AlonRoz05@users.noreply.github.com>
2023-02-08 17:48:42 +0200

    Create nnNumModel.py
    Create nnNumModel.py The model is trained to detect numbers (0-9).
    I also created helper_TMLFN.py  in this python files there are lots helper function for the nnNumModel.py

---
+++ helper_TMLFN.py
@@ -0,0 +1,61 @@
+import torch
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    accuracy = (correct / len(y_true)) * 100
+    return accuracy
+
+def print_train_time(start: float, end: float, device: torch.device = None):
+    total_time = end - start
+    print(f"Training time on {device}: {total_time:.3f} seconds")
+    return total_time
+
+def eval_model(model: torch.nn.Module, data_loader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, accuracy_fn):
+    loss, acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            y_pred = model(X)
+
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
+
+        loss /= len(data_loader)
+        acc /= len(data_loader)
+
+    return {"model_name": model.__class__.__name__,
+            "model_loss": loss.item(),
+            "model_accuracy": acc}
+
+def train_step(model: torch.nn.Module, data_loader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, optimizer: torch.optim.Optimizer, accuracy_fn, device):
+    train_loss, train_acc = 0, 0
+    model.train()
+    for batch, (X, y) in enumerate(data_loader):
+        X, y = X.to(device), y.to(device)
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss
+        train_acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    train_loss /= len(data_loader)
+    train_acc /= len(data_loader)
+    print(f"Train loss {train_loss:.5f} | Train accuracy {train_acc:.2f}")
+
+def test_step(model: torch.nn.Module, test_data_loader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, accuracy_fn, device):
+    test_loss, test_accuracy = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in test_data_loader:
+            X, y = X.to(device), y.to(device)
+            test_pred = model(X)
+
+            test_loss += loss_fn(test_pred, y)
+            test_accuracy += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+
+        test_loss /= len(test_data_loader)
+        test_accuracy /= len(test_data_loader)
+        print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_accuracy:.2f}\n")
---
+++ nnNumModel.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from timeit import default_timer as timer
+from tqdm.auto import tqdm
+from helper_TMLFN import accuracy_fn, print_train_time, eval_model, train_step, test_step
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+train_dataset = datasets.MNIST(root="./data",
+                                train=True, 
+                                download=True, 
+                                transform=torchvision.transforms.ToTensor(),
+                                target_transform=None)
+
+test_dataset = datasets.MNIST(root="./data",
+                                train=False, 
+                                download=True, 
+                                transform=torchvision.transforms.ToTensor(),
+                                target_transform=None)
+
+BATCH_SIZE = 32
+class_names = train_dataset.classes
+
+train_dataloader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_dataloader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+train_features_batch, train_labels_batch = next(iter(train_dataloader))
+
+class nnNumModel(nn.Module):
+    def __init__(self, input_size: int, hidden_units: int, output_size: int):
+        super().__init__()
+        self.layer_stack = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(input_size, hidden_units),
+            nn.Linear(hidden_units, hidden_units),
+            nn.Linear(hidden_units, output_size)
+        )
+
+    def forward(self, x):
+        return self.layer_stack(x)
+
+torch.manual_seed(42)
+model_1 = nnNumModel(input_size=28*28, hidden_units=8, output_size=len(class_names)).to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model_1.parameters(), lr=0.01)
+
+epochs = 10
+
+train_start_timer = timer()
+
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch {epoch}------------------------")
+    train_step(model=model_1, data_loader=train_dataloader, loss_fn=loss_fn, optimizer=optimizer, accuracy_fn=accuracy_fn, device=device)
+    test_step(model=model_1, test_data_loader=test_dataloader, loss_fn=loss_fn, accuracy_fn=accuracy_fn, device=device)
+
+train_end_timer = timer()
+
+total_train_time = print_train_time(train_start_timer, train_end_timer, device)
+model_result = eval_model(model_1, test_dataloader, loss_fn, accuracy_fn)
+print(model_result)
+
+
