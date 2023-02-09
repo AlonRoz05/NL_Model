335184f48da86098d17b0a077710bf0ef05e3116
AlonRoz <87911114+AlonRoz05@users.noreply.github.com>
2023-02-09 14:50:53 +0200

    Show model coolness
    Cleaner code and you can show what the model is predicting after you saved the model.

--- nnNumModel.py
+++ nnNumModel.py
@@ -49,25 +49,33 @@ model_1 = nnNumModel(input_size=28*28, hidden_units=8, output_size=len(class_nam
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model_1.parameters(), lr=0.01)
 
-train_start_timer = timer()
+train_model = False
+if train_model:
+    train_start_timer = timer()
 
-for epoch in tqdm(range(10)):
-    print(f"Epoch {epoch}\n------------------------")
-    train_step(model=model_1, data_loader=train_dataloader, loss_fn=loss_fn, optimizer=optimizer, accuracy_fn=accuracy_fn, device=device)
-    test_step(model=model_1, test_data_loader=test_dataloader, loss_fn=loss_fn, accuracy_fn=accuracy_fn, device=device)
+    for epoch in tqdm(range(10)):
+        print(f"Epoch {epoch}\n------------------------")
+        train_step(model=model_1, data_loader=train_dataloader, loss_fn=loss_fn, optimizer=optimizer, accuracy_fn=accuracy_fn, device=device)
+        test_step(model=model_1, test_data_loader=test_dataloader, loss_fn=loss_fn, accuracy_fn=accuracy_fn, device=device)
 
-train_end_timer = timer()
+    train_end_timer = timer()
 
-total_train_time = print_train_time(train_start_timer, train_end_timer, device)
+    total_train_time = print_train_time(train_start_timer, train_end_timer, device)
 
-img = Image.open("./test_img/img_1.jpg")
-img_tensor = ToTensor()(img).unsqueeze(0).to(device)
-model_choice = model_1(img_tensor).argmax(dim=1)
-print(f"The model's choice: {model_choice.item()}")
+    MODEL_PATH = Path("./models")
+    MODEL_PATH.mkdir(parents=True, exist_ok=True)
+    MODEL_NAME = "numModel_1.pth"
+    MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+    torch.save(model_1.state_dict(), MODEL_SAVE_PATH)
 
-MODEL_PATH = Path("./models")
-MODEL_PATH.mkdir(parents=True, exist_ok=True)
-MODEL_NAME = "numModel_1.pth"
-MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
-torch.save(model_1.state_dict(), MODEL_SAVE_PATH)
+else:
+    loaded_model_1 = nnNumModel(input_size=28*28, hidden_units=8, output_size=len(class_names))
+    loaded_model_1.load_state_dict(torch.load("./models/numModel_1.pth"))
+    loaded_model_1.to(device)
+
+    img = Image.open("./test_img/img_1.jpg")
+    img_tensor = ToTensor()(img).unsqueeze(0).to(device)
+    model_choice = loaded_model_1(img_tensor).argmax(dim=1)
+    print(f"The model's choice: {model_choice.item()}")
+
 
